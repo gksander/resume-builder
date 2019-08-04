6d9e524660f2578d85a443878397958ca7fdd46e
Grant Sander <gksander93@gmail.com>
2019-08-03 17:52:07 -0700

    Add site information

--- components/builder/BuilderInfo.vue
+++ components/builder/BuilderInfo.vue
@@ -1,7 +1,14 @@
 <template>
   <div>
-    <p>Information coming soon...</p>
-    <p>In the meantime, use the other tabs below.</p>
+    <p>
+      This site is a tool to generate nice-looking resumes with minimal work. You enter some information about yourself (name, work experience, etc.), select a resume template, and export the result as a PDF.
+    </p>
+    <p>
+      Use the tabs at the bottom of this panel to switch to data and style editing modes. Use the buttons in the top bar to import/export a resume from this site. You can save your resume as a PDF, or export the data as JSON. Any data exported from this site as JSON can be imported back in. (Data is stored in LocalStorage, so it will persist between sessions using the same browser.)
+    </p>
+    <p>
+      This is a site built by <a href="https://gksander.com" target="_blank" class="white--text">Grant Sander</a>. The code for this site is open source and can be found <a href="https://github.com/gksander/resume-builder" target="_blank" class="white--text">on GitHub</a>.
+    </p>
   </div>
 </template>
 
